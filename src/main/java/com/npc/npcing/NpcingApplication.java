c308209eb1fa950daf55262f47a2a6309373837c
code0613 <vhxmfltm3000@naver.com>
2025-02-17 15:32:00 +0900

    git init

---
+++ src/main/java/com/npc/npcing/NpcingApplication.java
@@ -0,0 +1,13 @@
+package com.npc.npcing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpcingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpcingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/npc/npcing/NpcingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.npc.npcing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NpcingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
